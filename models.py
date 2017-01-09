61649969fcb15fcdf0637b7e4936f5974fc5f3f0
morrillo <odoo@serverit08.aspasrl.com.ar>
2017-01-09 19:52:28 -0300

    Fixed big

--- models.py
+++ models.py
@@ -16,16 +16,24 @@ _logger = logging.getLogger(__name__)
 class mail_tracking_email(models.Model):
 	_inherit = 'mail.tracking.email'
 
-	@api.one
-	def compute_sender_partner_id(self):
-		if self.sender:
-			sender = self.sender
+	#@api.one
+	#def _compute_sender_partner_id(self):
+	#@api.one
+	#@api.onchange('sender')
+	@api.model
+	#def _compute_sender_partner_id(self):
+	def create(self, vals):
+		if 'sender' in vals.keys():
+			#import pdb;pdb.set_trace()
+			sender = vals['sender']
 			position_start = sender.find('<')
-			intermediate_sender = sender[position_start + 1]
+			intermediate_sender = sender[position_start + 1:]
 			final_sender = intermediate_sender[:len(intermediate_sender)-1]
 			partners = self.env['res.partner'].search([('email','=',final_sender)])
 			if partners:
-				self.sender_partner_id = partners[0].id
+				vals['sender_partner_id'] = partners[0].id
+		return super(mail_tracking_email, self).create(vals)
 
 	#sender_partner_id = fields.Many2one('res.partner',related='mail_message_id.author_id')
-	sender_partner_id = fields.Many2one('res.partner',related='mail_message_id.author_id')
+	#sender_partner_id = fields.Many2one('res.partner',compute=_compute_sender_partner_id,store=True)
+	sender_partner_id = fields.Many2one('res.partner')
