4600fa197172fecfb228b7cb9e8c3f47a3d84a68
morrillo <gustavo.orrillo@moldeo.coop>
2017-01-09 18:14:18 -0300

    Fixed bug

--- models.py
+++ models.py
@@ -16,4 +16,16 @@ _logger = logging.getLogger(__name__)
 class mail_tracking_email(models.Model):
 	_inherit = 'mail.tracking.email'
 
+	@api.one
+	def compute_sender_partner_id(self):
+		if self.sender:
+			sender = self.sender
+			position_start = sender.find('<')
+			intermediate_sender = sender[position_start + 1]
+			final_sender = intermediate_sender[:len(intermediate_sender)-1]
+			partners = self.env['res.partner'].search([('email','=',final_sender)])
+			if partners:
+				self.sender_partner_id = partners[0].id
+
+	#sender_partner_id = fields.Many2one('res.partner',related='mail_message_id.author_id')
 	sender_partner_id = fields.Many2one('res.partner',related='mail_message_id.author_id')
