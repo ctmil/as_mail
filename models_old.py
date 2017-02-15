5b613ee4cb34acf21269d442e7b6970adcd7d91d
morrillo <gustavo.orrillo@gmail.com>
2017-02-15 19:10:44 -0300

    Fixed bug

--- models_old.py
+++ models_old.py
@@ -26,7 +26,7 @@ class as_mail_message(osv.osv):
         	tools.sql.drop_view_if_exists(cr, 'as_mail_message')
 	        cr.execute("""
 			create view as_mail_message as 
-			select a.mail_message_id as message_id,a.res_partner_id as res_partner_id,b.author_id as author_id,b.message_type as message_type, 
+			select a.mail_message_id * a.res_partner_id as id,a.mail_message_id as mail_message_id,a.res_partner_id as res_partner_id,b.author_id as author_id,b.message_type as message_type, 
 			b.subject as subject, b.email_from as email_from,b.date as date from mail_message_res_partner_rel a inner join mail_message b on a.mail_message_id = b.id
 			where b.message_type in ('comment','email')
 	        	""")
